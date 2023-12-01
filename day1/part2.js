9e3b1655f2b2bd40c64fd3956461ce2b25e31ba8
Jake Rowsell <jake.rowsell@comparethemarket.com>
2023-12-01 15:27:37 +0000

    object.keys

--- day1/part2.js
+++ day1/part2.js
@@ -1,10 +1,28 @@
 const fs = require('fs')
 
 const input = fs.readFileSync('./input.txt', { encoding: 'utf-8' })
-const tokens = ['1','2','3','4','5','6','7','8','9','one','two','three','four','five','six','seven','eight','nine']
 const numberMap = {
-    '1': '1'    ,'2': '2'    ,'3': '3'    ,'4': '4'    ,'5': '5'    ,'6': '6'    ,'7': '7'    ,'8': '8'    ,'9': '9'    ,'one': '1'    ,'two': '2'    ,'three': '3'    ,'four': '4'    ,'five': '5'    ,'six': '6'    ,'seven': '7'    ,'eight': '8'    ,'nine': '9'    ,
+    '1': '1',
+    '2': '2',
+    '3': '3',
+    '4': '4',
+    '5': '5',
+    '6': '6',
+    '7': '7',
+    '8': '8',
+    '9': '9',
+    'one': '1',
+    'two': '2',
+    'three': '3',
+    'four': '4',
+    'five': '5',
+    'six': '6',
+    'seven': '7',
+    'eight': '8',
+    'nine': '9',
 }
+const tokens = Object.keys(numberMap)
+
 function getDigitSum (line) {
     const includedTokens = tokens
         .map(t => ({token: t, firstIndex: line.indexOf(t), lastIndex: line.lastIndexOf(t)})) // { token: 'a', index: 1 }
